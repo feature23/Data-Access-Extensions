0596474316ca468e2684f92771961d5b52637f98
Paul Irwin <paulirwin@gmail.com>
2015-02-20 14:37:37 -0500

    Params overloading issue

--- src/F23.DataAccessExtensions.EntityFramework/DatabaseExtensions.cs
+++ src/F23.DataAccessExtensions.EntityFramework/DatabaseExtensions.cs
@@ -17,6 +17,19 @@ namespace F23.DataAccessExtensions
         /// <typeparam name="TEntity">The type of result to return.</typeparam>
         /// <param name="database">The Entity Framework Database instance.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns a list of results.</returns>
+        public static IList<TEntity> ExecuteSproc<TEntity>(this Database database, string sprocName)
+            where TEntity : class, new()
+        {
+            return database.Connection.ExecuteSproc<TEntity>(database.GetUnderlyingTransaction(), sprocName);
+        }
+
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns a list of results.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of result to return.</typeparam>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Returns a list of results.</returns>
         public static IList<TEntity> ExecuteSproc<TEntity>(this Database database, string sprocName, params IDbDataParameter[] parameters)
@@ -45,6 +58,19 @@ namespace F23.DataAccessExtensions
         /// <typeparam name="TEntity">The type of result to return.</typeparam>
         /// <param name="database">The Entity Framework Database instance.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns a list of results.</returns>
+        public static Task<IList<TEntity>> ExecuteSprocAsync<TEntity>(this Database database, string sprocName)
+            where TEntity : class, new()
+        {
+            return database.Connection.ExecuteSprocAsync<TEntity>(database.GetUnderlyingTransaction(), sprocName);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns a list of results.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of result to return.</typeparam>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Asynchronously returns a list of results.</returns>
         public static Task<IList<TEntity>> ExecuteSprocAsync<TEntity>(this Database database, string sprocName, params IDbDataParameter[] parameters)
@@ -65,6 +91,18 @@ namespace F23.DataAccessExtensions
             where TEntity : class, new()
         {
             return database.Connection.ExecuteSprocAsync<TEntity>(database.GetUnderlyingTransaction(), sprocName, parameters);
+        }
+        
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns a list with the first column of results.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the first column of results.</typeparam>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns a list of the values in the first column of results.</returns>
+        public static IList<TResult> ExecuteSprocSingleColumn<TResult>(this Database database, string sprocName)
+        {
+            return database.Connection.ExecuteSprocSingleColumn<TResult>(database.GetUnderlyingTransaction(), sprocName);
         }
 
         /// <summary>
@@ -99,6 +137,18 @@ namespace F23.DataAccessExtensions
         /// <typeparam name="TResult">The type of the first column of results.</typeparam>
         /// <param name="database">The Entity Framework Database instance.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns a list of the values in the first column of results.</returns>
+        public static Task<IList<TResult>> ExecuteSprocSingleColumnAsync<TResult>(this Database database, string sprocName)
+        {
+            return database.Connection.ExecuteSprocSingleColumnAsync<TResult>(database.GetUnderlyingTransaction(), sprocName);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns a list with the first column of results.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the first column of results.</typeparam>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Asynchronously returns a list of the values in the first column of results.</returns>
         public static Task<IList<TResult>> ExecuteSprocSingleColumnAsync<TResult>(this Database database, string sprocName, params IDbDataParameter[] parameters)
@@ -117,6 +167,17 @@ namespace F23.DataAccessExtensions
         public static Task<IList<TResult>> ExecuteSprocSingleColumnAsync<TResult>(this Database database, string sprocName, params Parameter[] parameters)
         {
             return database.Connection.ExecuteSprocSingleColumnAsync<TResult>(database.GetUnderlyingTransaction(), sprocName, parameters);
+        }
+
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns the number of rows affected.
+        /// </summary>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns the number of rows affected.</returns>
+        public static int ExecuteSprocNonQuery(this Database database, string sprocName)
+        {
+            return database.Connection.ExecuteSprocNonQuery(database.GetUnderlyingTransaction(), sprocName);
         }
 
         /// <summary>
@@ -148,6 +209,17 @@ namespace F23.DataAccessExtensions
         /// </summary>
         /// <param name="database">The Entity Framework Database instance.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns the number of rows affected.</returns>
+        public static Task<int> ExecuteSprocNonQueryAsync(this Database database, string sprocName)
+        {
+            return database.Connection.ExecuteSprocNonQueryAsync(database.GetUnderlyingTransaction(), sprocName);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns the number of rows affected.
+        /// </summary>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Asynchronously returns the number of rows affected.</returns>
         public static Task<int> ExecuteSprocNonQueryAsync(this Database database, string sprocName, params IDbDataParameter[] parameters)
@@ -165,6 +237,17 @@ namespace F23.DataAccessExtensions
         public static Task<int> ExecuteSprocNonQueryAsync(this Database database, string sprocName, params Parameter[] parameters)
         {
             return database.Connection.ExecuteSprocNonQueryAsync(database.GetUnderlyingTransaction(), sprocName, parameters);
+        }
+        
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns the SQL XML document of the first column of the first row returned.
+        /// </summary>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns the XDocument of the SQL XML value of the first column of the first row.</returns>
+        public static XDocument ExecuteSprocXmlScalar(this Database database, string sprocName)
+        {
+            return database.Connection.ExecuteSprocXmlScalar(database.GetUnderlyingTransaction(), sprocName);
         }
 
         /// <summary>
@@ -189,6 +272,17 @@ namespace F23.DataAccessExtensions
         public static XDocument ExecuteSprocXmlScalar(this Database database, string sprocName, params Parameter[] parameters)
         {
             return database.Connection.ExecuteSprocXmlScalar(database.GetUnderlyingTransaction(), sprocName, parameters);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns the SQL XML document of the first column of the first row returned.
+        /// </summary>
+        /// <param name="database">The Entity Framework Database instance.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns the XDocument of the SQL XML value of the first column of the first row.</returns>
+        public static Task<XDocument> ExecuteSprocXmlScalarAsync(this Database database, string sprocName)
+        {
+            return database.Connection.ExecuteSprocXmlScalarAsync(database.GetUnderlyingTransaction(), sprocName);
         }
 
         /// <summary>
--- src/F23.DataAccessExtensions.EntityFramework/DbContextExtensions.cs
+++ src/F23.DataAccessExtensions.EntityFramework/DbContextExtensions.cs
@@ -20,6 +20,19 @@ namespace F23.DataAccessExtensions
         /// <typeparam name="TEntity">The type of result to return.</typeparam>
         /// <param name="context">The Entity Framework context.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns a list of results.</returns>
+        public static IList<TEntity> ExecuteSproc<TEntity>(DbContext context, string sprocName)
+            where TEntity : class, new()
+        {
+            return context.Database.ExecuteSproc<TEntity>(sprocName);
+        }
+
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns a list of results.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of result to return.</typeparam>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Returns a list of results.</returns>
         public static IList<TEntity> ExecuteSproc<TEntity>(DbContext context, string sprocName, params IDbDataParameter[] parameters)
@@ -40,6 +53,19 @@ namespace F23.DataAccessExtensions
             where TEntity : class, new()
         {
             return context.Database.ExecuteSproc<TEntity>(sprocName, parameters);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns a list of results.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of result to return.</typeparam>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns a list of results.</returns>
+        public static Task<IList<TEntity>> ExecuteSprocAsync<TEntity>(DbContext context, string sprocName)
+            where TEntity : class, new()
+        {
+            return context.Database.ExecuteSprocAsync<TEntity>(sprocName);
         }
 
         /// <summary>
@@ -76,6 +102,18 @@ namespace F23.DataAccessExtensions
         /// <typeparam name="TResult">The type of the first column of results.</typeparam>
         /// <param name="context">The Entity Framework context.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns a list of the values in the first column of results.</returns>
+        public static IList<TResult> ExecuteSprocSingleColumn<TResult>(DbContext context, string sprocName)
+        {
+            return context.Database.ExecuteSprocSingleColumn<TResult>(sprocName);
+        }
+
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns a list with the first column of results.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the first column of results.</typeparam>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Returns a list of the values in the first column of results.</returns>
         public static IList<TResult> ExecuteSprocSingleColumn<TResult>(DbContext context, string sprocName, params IDbDataParameter[] parameters)
@@ -94,6 +132,18 @@ namespace F23.DataAccessExtensions
         public static IList<TResult> ExecuteSprocSingleColumn<TResult>(DbContext context, string sprocName, params Parameter[] parameters)
         {
             return context.Database.ExecuteSprocSingleColumn<TResult>(sprocName, parameters);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns a list with the first column of results.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the first column of results.</typeparam>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns a list of the values in the first column of results.</returns>
+        public static Task<IList<TResult>> ExecuteSprocSingleColumnAsync<TResult>(DbContext context, string sprocName)
+        {
+            return context.Database.ExecuteSprocSingleColumnAsync<TResult>(sprocName);
         }
 
         /// <summary>
@@ -127,6 +177,17 @@ namespace F23.DataAccessExtensions
         /// </summary>
         /// <param name="context">The Entity Framework context.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns the number of rows affected.</returns>
+        public static int ExecuteSprocNonQuery(DbContext context, string sprocName)
+        {
+            return context.Database.ExecuteSprocNonQuery(sprocName);
+        }
+
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns the number of rows affected.
+        /// </summary>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Returns the number of rows affected.</returns>
         public static int ExecuteSprocNonQuery(DbContext context, string sprocName, params IDbDataParameter[] parameters)
@@ -144,6 +205,17 @@ namespace F23.DataAccessExtensions
         public static int ExecuteSprocNonQuery(DbContext context, string sprocName, params Parameter[] parameters)
         {
             return context.Database.ExecuteSprocNonQuery(sprocName, parameters);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns the number of rows affected.
+        /// </summary>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns the number of rows affected.</returns>
+        public static Task<int> ExecuteSprocNonQueryAsync(DbContext context, string sprocName)
+        {
+            return context.Database.ExecuteSprocNonQueryAsync(sprocName);
         }
 
         /// <summary>
@@ -175,6 +247,17 @@ namespace F23.DataAccessExtensions
         /// </summary>
         /// <param name="context">The Entity Framework context.</param>
         /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Returns the XDocument of the SQL XML value of the first column of the first row.</returns>
+        public static XDocument ExecuteSprocXmlScalar(DbContext context, string sprocName)
+        {
+            return context.Database.ExecuteSprocXmlScalar(sprocName);
+        }
+
+        /// <summary>
+        /// Executes a stored procedure via RPC and returns the SQL XML document of the first column of the first row returned.
+        /// </summary>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
         /// <param name="parameters">Parameters to pass to the stored procedure.</param>
         /// <returns>Returns the XDocument of the SQL XML value of the first column of the first row.</returns>
         public static XDocument ExecuteSprocXmlScalar(DbContext context, string sprocName, params IDbDataParameter[] parameters)
@@ -192,6 +275,17 @@ namespace F23.DataAccessExtensions
         public static XDocument ExecuteSprocXmlScalar(DbContext context, string sprocName, params Parameter[] parameters)
         {
             return context.Database.ExecuteSprocXmlScalar(sprocName, parameters);
+        }
+
+        /// <summary>
+        /// Asynchronously executes a stored procedure via RPC and returns the SQL XML document of the first column of the first row returned.
+        /// </summary>
+        /// <param name="context">The Entity Framework context.</param>
+        /// <param name="sprocName">The name of the stored procedure to execute.</param>
+        /// <returns>Asynchronously returns the XDocument of the SQL XML value of the first column of the first row.</returns>
+        public static Task<XDocument> ExecuteSprocXmlScalarAsync(DbContext context, string sprocName)
+        {
+            return context.Database.ExecuteSprocXmlScalarAsync(sprocName);
         }
 
         /// <summary>
